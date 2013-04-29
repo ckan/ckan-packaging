82b6556edbd92fef845c4ce96b84454808407e61
Joe Tsoi <joe.yeung.tsoi@gmail.com>
2013-04-29 16:07:38 +0100

    fix typos!

--- tmp/after_web.sh
+++ tmp/after_web.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-sed -i 's/^Listen.*/Listen 8080/g' /etc/apache2/conf/ports.conf
+sed -i 's/^Listen.*/Listen 8080/g' /etc/apache2/ports.conf
 update-rc.d nginx defaults
-update-rc.d apache defaults
+update-rc.d apache2 defaults
 initctl reload-configuration
 rm -f /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/ckan /etc/nginx/sites-enabled/ckan
 a2dissite default
-a2ensite ckan
-
 
 service apache2 restart
 service nginx restart
