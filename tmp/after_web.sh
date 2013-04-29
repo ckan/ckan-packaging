6de9f7e8bd592abd6e018f563f9dda323024ae4c
Joe Tsoi <joe.yeung.tsoi@gmail.com>
2013-04-29 15:44:08 +0100

    ubuntuify after_web.sh script

--- tmp/after_web.sh
+++ tmp/after_web.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-sed -i 's/^Listen.*/Listen 8080/g' /etc/httpd/conf/httpd.conf
+sed -i 's/^Listen.*/Listen 8080/g' /etc/apache2/conf/ports.conf
 chkconfig --level 2345 nginx on
-chkconfig --level 2345 httpd on
-setsebool -P httpd_can_network_connect 1
-setsebool -P httpd_tmp_exec on
+chkconfig --level 2345 apache on
 initctl reload-configuration
-rm -f /etc/nginx/conf.d/default.conf
-rm -f /etc/httpd/conf.d/welcome.conf
+rm -f /etc/nginx/sites-enabled/default
+rm -f /etc/apache2/sites-enabled/000-default
 
-service httpd restart
+service apache2 restart
 service nginx restart
