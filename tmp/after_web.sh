d023541db7292601849b906bb9b96f7728c68791
Joe Tsoi <joe.yeung.tsoi@gmail.com>
2013-04-29 15:53:33 +0100

    fix after_web

--- tmp/after_web.sh
+++ tmp/after_web.sh
@@ -5,7 +5,10 @@ chkconfig --level 2345 nginx on
 chkconfig --level 2345 apache on
 initctl reload-configuration
 rm -f /etc/nginx/sites-enabled/default
-rm -f /etc/apache2/sites-enabled/000-default
+ln -s /etc/nginx/sites-available/ckan /etc/nginx/sites-enabled/ckan
+a2dissite default
+a2ensite ckan
+
 
 service apache2 restart
 service nginx restart
